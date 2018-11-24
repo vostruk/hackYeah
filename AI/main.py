1c4a6d3fdd004dd42ef50e1a0e08be40532ddf9b
Vladimir Ostruk <vovaostruk@hotmail.com>
2018-11-24 16:50:34 +0100

    docker configuration

--- AI/main.py
+++ AI/main.py
@@ -58,6 +58,6 @@ def new_train_item():
 
 if __name__ == '__main__':
     if os.environ['ENVIRONMENT'] == 'production':
-        app.run(port=80,host='0.0.0.0')
+        app.run(port=5000,host='0.0.0.0')
     if os.environ['ENVIRONMENT'] == 'local':
         app.run(port=5000,host='0.0.0.0')
